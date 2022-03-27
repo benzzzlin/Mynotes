2f07c8a309e8fc23763d7638c03ad4ac6d442517
benzzzlin <linlibin0930@163.com>
2022-03-27 21:21:13 +0800

    feat:增加main函数功能

--- 7_code/src/main.c
+++ 7_code/src/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+void exchage(int *a, int *b)
+{
+    int temp = *b;
+    *b = *a;
+    *a = temp;
+}
 int main()
 {
+    int a = 1;
+    int b = 2;
+    int temp = 0;
+    exchage(&a, &b);
+    printf("a = %d b = %d \n", a, b);
     printf("hello world!\n");
+    return 0; 
 }
